e6c42211e45cd9b86107c2f136e8acd8d54fc1ed
xnought <bertuccd@oregonstate.edu>
2023-12-21 21:53:35 -0800

    feat: generate all possible child combinations

--- asm_index.py
+++ asm_index.py
@@ -1,8 +1,28 @@
+class Node:
+    def __init__(self, data, parent=None, children=[]):
+        self.data = data
+        self.parent = parent
+        self.children = children
+
+    def __repr__(self):
+        return f"{self.data}"
+
+
 def str_atomize(s: str) -> list[str]:
     """atomize: break a string into its atoms, for us this means characters (bytes)
     for more complicated objects, will need more complicated atomize functions
     """
     return [c for c in s]
+
+
+def str_asm_combos(a: str, others: list[str]) -> list[str]:
+    """str_asm_combos: possible left and right side concatenations of string assembly"""
+    return list(set([a + o for o in others] + [o + a for o in others]))
+
+
+def possible_children(node: Node, pool: list[str]) -> list[Node]:
+    """possible_children: given a node, return all possible children"""
+    return [Node(data=p, parent=node) for p in str_asm_combos(node.data, pool)]
 
 
 def str_asm_index(s: str) -> int:
@@ -16,6 +36,19 @@ def str_asm_index(s: str) -> int:
     # can't assemble nothing or a single atom (one atom is already assembled)
     if len(atoms) <= 1:
         return 0
+    # two objects can be assembled in one step
+    elif len(atoms) == 2:
+        return 1
+    # three objects can be assembled in two step
+    elif len(atoms) == 3:
+        return 2
+
+    # but anything over three is not clear what the assembly index is
+    # so we have to combinatorially try stuff
+    pool = [] + atoms
+    root = Node(data=atoms[0])
+    potentials = possible_children(root, pool)
+    print(potentials)
 
     return float("inf")
 

e6eedaf4930a3e12f34a2c4ed1e54e806fe9158b
xnought <bertuccd@oregonstate.edu>
2023-12-21 20:06:03 -0800

    feat: setup testing

--- asm_index.py
+++ asm_index.py
@@ -1,2 +1,26 @@
+def str_asm_index(s: str) -> int:
+    """a_i: assembly index with smallest assembly path
+
+    Returns:
+        int: the asm index of the string
+    """
+    return float("inf")
+
+
 if __name__ == "__main__":
-    print("Hello assembly index world!")
+    s1 = "A"
+    s2 = "AB"
+    s3 = "ABAB"
+    s4 = "ABRACADABRA"
+
+    assert str_asm_index(s1) == 0
+    print("PASSED TEST 1")
+
+    assert str_asm_index(s2) == 1
+    print("PASSED TEST 2")
+
+    assert str_asm_index(s3) == 2
+    print("PASSED TEST 3")
+
+    assert str_asm_index(s4) == 7
+    print("PASSED TEST 4")

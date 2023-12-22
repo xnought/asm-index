7332670b8c50cbe979e4315e59390980075ae4b9
xnought <bertuccd@oregonstate.edu>
2023-12-21 23:31:58 -0800

    feat: finished with naive bfs algo

--- asm_index.py
+++ asm_index.py
@@ -1,8 +1,9 @@
 class Node:
-    def __init__(self, data, parent=None, children=[]):
+    def __init__(self, data, parent=None, children=[], is_atom=False):
         self.data = data
         self.parent = parent
         self.children = children
+        self.is_atom = is_atom
 
     def __repr__(self):
         return f"{self.data}"
@@ -10,12 +11,7 @@ class Node:
 
 def str_unique(strs: list[str]) -> list[str]:
     """unique: return a list of unique strings"""
-    uniq = [strs[0]]
-    for s in strs[1:]:
-        if s not in uniq:
-            uniq.append(s)
-
-    return list(reversed(uniq))
+    return list(set(strs))
 
 
 def str_atomize(s: str) -> list[str]:
@@ -30,36 +26,25 @@ def str_asm_combos(a: str, others: list[str]) -> list[str]:
     return str_unique([a + o for o in others] + [o + a for o in others])
 
 
-# def plausible_asms(
-#     node: Node, pool: list[str], solution: str, tried: list[str]
-# ) -> list[Node]:
-#     """possible_children: given a node, return all possible children"""
-#     # adds all combos and if the combos are definitely wrong, (like a substring that never shows up, then we're soooo wrong and don't need to add)
-#     return [
-#         Node(data=p, parent=node)
-#         for p in str_asm_combos(node.data, pool)
-#         if p in solution
-#         and len(p) <= len(solution)
-#         and p != node.data
-#         and p not in tried
-#     ]
-
-
 def already_asmed(node: Node):
     # recur backwards up till we hit parent of None
-    if node.parent is None:
+    if node.is_atom:
         return []
 
     return already_asmed(node.parent) + [node.data]
 
 
 def backtrack_asm_index(node: Node, print_path=False):
-    if print_path:
-        print(node.data)
-    if node.parent is None:
-        return 0
+    solution = []
 
-    return backtrack_asm_index(node.parent) + 1
+    def _backtrack(node: Node):
+        solution.append(node.data)
+        if node.is_atom:
+            return
+        _backtrack(node.parent)
+
+    _backtrack(node)
+    return list(reversed(solution))
 
 
 def str_asm_index(s: str) -> int:
@@ -70,22 +55,15 @@ def str_asm_index(s: str) -> int:
     """
     # can't assemble nothing or a single atom (one atom is already assembled)
     if len(s) <= 1:
-        return 0
-    # two objects can be assembled in one step
-    elif len(s) == 2:
-        return 1
-    # three objects can be assembled in two step
-    elif len(s) == 3:
-        return 2
+        return [s]
 
     atoms = str_atomize(s)
 
     # but anything over three is not clear what the assembly index is
     # so we have to combinatorially try stuff
-    root = Node(data=atoms[0])
-    cur = root
-    q = [root]
-
+    uber_root = Node(data=None, parent=None)
+    uber_root.children = [Node(data=a, parent=uber_root, is_atom=True) for a in atoms]
+    q = [c for c in uber_root.children]
     while len(q) > 0:
         cur = q.pop(0)
 
@@ -93,15 +71,12 @@ def str_asm_index(s: str) -> int:
         pool = atoms + already_asmed(cur)  # can use already assembled for cost of 1
         for new_asm in str_asm_combos(cur.data, pool):
             if new_asm == s:
-                return backtrack_asm_index(Node(data=new_asm, parent=cur))
-            elif (
-                len(new_asm) <= len(s)
-                and new_asm in s
-                and new_asm not in [n.data for n in q]
-            ):
+                res = backtrack_asm_index(Node(data=new_asm, parent=cur))
+                return res
+            elif len(new_asm) <= len(s) and new_asm in s:
                 q.append(Node(data=new_asm, parent=cur))
 
-    return float("inf")
+    return []
 
 
 if __name__ == "__main__":
@@ -110,14 +85,14 @@ if __name__ == "__main__":
     s3 = "ABAB"
     s4 = "ABRACADABRA"
 
-    # assert str_asm_index(s1) == 0
-    # print("PASSED TEST 1")
+    print(str_asm_index(s1))
+    print("PASSED TEST 1")
 
-    # assert str_asm_index(s2) == 1
-    # print("PASSED TEST 2")
+    print(str_asm_index(s2))
+    print("PASSED TEST 2")
 
-    # assert str_asm_index(s3) == 2
-    # print("PASSED TEST 3")
+    print(str_asm_index(s3))
+    print("PASSED TEST 3")
 
-    assert str_asm_index(s4) == 7
+    print(str_asm_index(s4))
     print("PASSED TEST 4")
